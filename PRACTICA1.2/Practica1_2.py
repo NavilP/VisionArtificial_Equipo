4ca0bfc7435cd83077b50dd61f819ceaa5d5f45e
NavilPinedaRugerio <navyadenip@gmail.com>
2023-09-13 22:15:26 -0600

    Funciones

---
+++ PRACTICA1.2/Practica1_2.py
@@ -0,0 +1,28 @@
+"""
+    Práctica 1.2: Análisis Inicial Imagen Digital
+    Vision Artificial
+    Equipo:
+    Miguel Angel Sanchez Zanjuampa
+    Navil Pineda Rugerio
+"""
+
+# Librerias
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Abrir imagen
+ruta = 'c:/5TO_UPIIT/VISION_ARTIFICIAL/PRACTICA1/IMAGES/ldscp.jpg'
+img = cv2.imread(ruta, 1)
+cv2.imshow('Imagen Original', img)
+
+# Funcion para histograma de imagen
+
+# Funcion para convertir a escala de grises
+
+# Mostrar histograma
+
+# Graficar valores de intensidad de 5 filas
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
