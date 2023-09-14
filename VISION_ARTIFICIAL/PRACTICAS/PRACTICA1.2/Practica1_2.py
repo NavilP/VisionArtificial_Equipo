5ac6ca83d623987f133581d18b2abd86ceb675f8
NavilPinedaRugerio <navyadenip@gmail.com>
2023-09-14 00:28:48 -0600

    Cambio a notebook

--- VISION_ARTIFICIAL/PRACTICAS/PRACTICA1.2/Practica1_2.py
+++ VISION_ARTIFICIAL/PRACTICAS/PRACTICA1.2/Practica1_2.py
@@ -17,8 +17,43 @@ img = cv2.imread(ruta, 1)
 cv2.imshow('Imagen Original', img)
 
 # Funcion para histograma de imagen
+def histograma(imagen, canal):
+    hght, wid, channel = img.shape
+    count =[]
+    r = []
+     
+    for k in range(0, 255):
+        r.append(k)
+        count1 = 0
+         
+        for i in range(hght):
+            for j in range(wid):
+                if img[i, j, canal] == k:
+                    count1+= 1
+        count.append(count1)
+         
+    return (r, count)
+
+r, count = histograma(np.copy(img), 0) # Canal rojo
+
+
+# Funcion para tomar un fragmento de la imagen
 
 # Funcion para convertir a escala de grises
+def escala_grises(imagen):
+    wid, hght, canal = imagen.shape
+    matriz = np.copy(imagen)
+    for i in range(hght):
+        for j in range(wid):
+            pixel = imagen[j][i]
+            gris = (pixel[0] * 0.299) + (pixel[1] * 0.587) + (pixel[2] * 0.114)
+            matriz[j][i][0] = gris
+            matriz[j][i][1] = gris
+            matriz[j][i][2] = gris
+    return matriz
+grisesImg = escala_grises(np.copy(img))
+cv2.imshow('Imagen en escala de grises', grisesImg)
+
 
 # Mostrar histograma
 
